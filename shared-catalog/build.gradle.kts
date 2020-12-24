f5bab830621a69f23c34873a6f0c78713cc83638
Cedric Champeau <cedric.champeau@gmail.com>
2020-12-24 11:43:26 +0100

    Use a precompiled settings script plugin

--- shared-catalog/build.gradle.kts
+++ shared-catalog/build.gradle.kts
@@ -1,12 +1,3 @@
 plugins {
     `kotlin-dsl`
 }
-
-gradlePlugin {
-    plugins {
-        create("sharedCatalog") {
-            id = "org.gradle.my-catalog"
-            implementationClass = "org.gradle.MyCatalogPlugin"
-        }
-    }
-}
---
+++ shared-catalog/src/main/kotlin/org.gradle.my-catalog.settings.gradle.kts
@@ -0,0 +1,12 @@
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            alias("junit-api").to("org.junit.jupiter:junit-jupiter-api:5.7.0")
+            alias("junit-engine").to("org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
+
+            alias("commons-math3").to("org.apache.commons:commons-math3:3.6.1")
+
+            alias("guava").to("com.google.guava:guava:30.0-jre")
+        }
+    }
+}
